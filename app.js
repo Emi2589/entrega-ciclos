0ce5335937e933c913488d961fb2506fea6e80ba
Emiliano Pulen <Pulen.e@hotmail.com>
2022-08-31 22:05:15 -0300

    entrega ciclos

---
+++ app.js
@@ -0,0 +1,11 @@
+let numero = Number(prompt("ingrese los minutos que usted lee por dia"));
+
+for (let i = 1; i <= 30; i++) {
+  let resultado = numero * i;
+  console.log(`${numero} x ${i} = ${resultado}`);
+  
+  
+}
+
+
+
